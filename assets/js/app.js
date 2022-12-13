6c14e56118c29092bf990a24e8724dd7170e8c78
jovanny <jovanny.valencia@est.iudigital.edu.co>
2022-12-13 00:52:55 -0500

    mi primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import { productsDetails } from "./productos";
+
+//1. Cree un nuevo array basado en products que solo tenga los nombres de los productos
+//2. Cree una funcion para filtar los productos con descuentos mayores al 12%
+//3. Cree una funcion que retorne la cantidad de stock existente en los productos de la categoria smartphones
+//4. En un nuevo array cambie el nombre de la categoria fragrances por parfums
+//5. Cree un nuevo array con los productos ordenados por precio.
